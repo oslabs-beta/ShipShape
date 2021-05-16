be6726bc19812c34468d6d547b3010df0978a2ba
Brian Barr <brian.barr23@gmail.com>
2021-05-16 16:12:06 -0400

    add + remove console log to test controller locally

--- server/controller/nodeController.js
+++ server/controller/nodeController.js
@@ -4,7 +4,8 @@ const k8sApi = require('./../k8sApi');
 const nodeController = {};
 
 nodeController.getNodes = async function(req, res, next){
-    const data = await k8sApi.listNode('default');
+  
+  const data = await k8sApi.listNode('default');
     console.log(data);
     res.locals.nodes = data;
     return next();
