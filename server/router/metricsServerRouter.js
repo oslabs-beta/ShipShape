5d01495426cdff64d23d4fcad48c1e1eeb4810ae
Whit Rooke <whit.rooke@gmail.com>
2021-05-17 17:07:51 -0400

    added backend functionality to get snapshot of live metrics data using metrics server

--- server/router/metricsServerRouter.js
+++ server/router/metricsServerRouter.js
@@ -1,12 +1,18 @@
 const cmd = require('node-cmd');
 const express = require('express');
-const nodeController = require('./../controller/nodeController.js')
+const nodeController = require('./../controller/nodeController.js');
+const podController = require('./../controller/podController.js');
 
 const metricsServerRouter = express.Router();
 
 metricsServerRouter.get('/nodes', nodeController.getNodeMetrics, (req, res) => {
-  console.log(res.locals.nodes.metrics);
-  res.status(200).json(res.locals.nodes.metrics);
+  res.status(200).json(res.locals.nodeMetrics);
 })
+
+metricsServerRouter.get('/pods', podController.getPodMetrics, (req, res) =>{
+  res.status(200).json(res.locals.podMetrics);
+})
+
+
 
 module.exports = metricsServerRouter;
