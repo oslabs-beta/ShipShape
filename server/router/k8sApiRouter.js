ac47b8460b275d8b767e73846ca391cfaf0acbec
Whit Rooke <whit.rooke@gmail.com>
2021-05-18 15:13:32 -0400

    changed constructors to be objects with asynchronous methods that return specific data to work better with resolving graphQL requests

--- server/router/k8sApiRouter.js
+++ server/router/k8sApiRouter.js
@@ -6,19 +6,23 @@ const podController = require('./../controller/podController');
 
 const k8sApiRouter = express.Router();
 
-k8sApiRouter.get('/nodes', namespaceController.getNamespaces, nodeController.getNodes, (req, res) =>{
-  console.log(res.locals.nodes);
-  res.status(200).json(res.locals.nodes)
-})
+// COMMENTED OUT BECAUSE USES OLD NODE CONSTRUCTOR, CHANGED TO OBJECT WITH CONSTRUCTION METHODS
+
+// k8sApiRouter.get('/nodes', namespaceController.getNamespaces, nodeController.getNodes, (req, res) =>{
+//   console.log(res.locals.nodes);
+//   res.status(200).json(res.locals.nodes)
+// })
 
 k8sApiRouter.get('/namespaces', namespaceController.getNamespaces, (req, res) =>{
   console.log(res.locals.namespaces);
   res.status(200).json(res.locals.namespaces);
 })
 
-k8sApiRouter.get('/pods', namespaceController.getNamespaces, podController.getPods, (req, res) =>{
-  console.log(res.locals.pods);
-  res.status(200).json(res.locals.pods);
-})
+// COMMENTED OUT BECAUSE USES OLD POD CONSTRUCTOR, CHANGED TO OBJECT WITH CONSTRUCTION METHODS
+
+// k8sApiRouter.get('/pods', namespaceController.getNamespaces, podController.getPods, (req, res) =>{
+//   console.log(res.locals.pods);
+//   res.status(200).json(res.locals.pods);
+// })
 
 module.exports = k8sApiRouter
