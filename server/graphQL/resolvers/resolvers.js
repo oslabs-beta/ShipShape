b17733f9d2f7dbb4dc810a28ce94dc07576142d3
Brian Barr <brian.barr23@gmail.com>
2021-05-18 11:44:42 -0400

    imporved resolvers to combine data

--- server/graphQL/resolvers/resolvers.js
+++ server/graphQL/resolvers/resolvers.js
@@ -31,6 +31,8 @@ module.exports = {
       //should be refactored using only forEach, or better yet a findOne style method would improve performance;
       // I also feel like we should fold container status into this query as well
       // question though - how closely do we want to match original data source? This could allow us to build a graphQL tool
+      // maybe use a lodash function https://lodash.com/docs/4.17.15; this will likely have a similar time complexity,
+      // but it will be more imperative and easy to read.
       const podArray = []
       podsApi.forEach((pod) => {
         const mergedPod = podsCmd.reduce((original, metrics) => {
