5be8b53e5bdc8ac78746546513558d50f9d6d9e2
Rebecca Schell <rebeccaschell503@gmail.com>
2021-05-18 11:42:46 -0400

    pulling in Whits changes

--- client/Components/Home/Cluster.jsx
+++ client/Components/Home/Cluster.jsx
@@ -1,38 +1,27 @@
-import React, { useState } from 'react';
-import { FaAngleDown } from 'react-icons/fa'
-
-
+import React, { useState } from "react";
+import { FaAngleDown } from "react-icons/fa";
 
 const Cluster = () => {
+  const [clicked, setClicked] = useState(false);
 
-    const [clicked, setClicked] = useState(false);
-
-    return(
-        <div  >
-            <button className='btnDashboard ah' onClick={() => setClicked(!clicked)}>
-                Cluster
-                <FaAngleDown />
-            </button>
-            {
-                clicked ? (
-                    <div className='dropDown'>
-                        <ul>
-                        <li>Hello World</li>
-                        <li>Hello World</li>
-                        <li>Hello World</li>
-                        <li>Hello World</li>
-                        </ul>
-                        
-                    </div>
-                ) : (
-                    null
-                )
-                
-            }
-            
-           
+  return (
+    <div>
+      <button className="btnDashboard ah" onClick={() => setClicked(!clicked)}>
+        Pod's View
+        <FaAngleDown />
+      </button>
+      {clicked ? (
+        <div className="dropDown">
+          <ul>
+            <li>Hello World</li>
+            <li>Hello World</li>
+            <li>Hello World</li>
+            <li>Hello World</li>
+          </ul>
         </div>
-    )
+      ) : null}
+    </div>
+  );
 };
 
 export default Cluster;
