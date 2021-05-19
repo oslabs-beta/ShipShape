4b00c1272a7a89573c67a1ca818110e4c1ede6c9
Brian Barr <brian.barr23@gmail.com>
2021-05-19 11:08:47 -0400

    fixed mock data flow

--- server/graphQL/resolvers/resolvers.js
+++ server/graphQL/resolvers/resolvers.js
@@ -1,12 +1,17 @@
 const _ = require('lodash');
-const mock = require('../mockData/mockData.js');
+const mockPods = require('../mockData/rawPods.json');
+const mockPodMetrics = require('../mockData/rawPodMetrics.json')
+const mockNodes = require('../mockData/rawNodeShort.json')
+const nodeMetrics = require('../mockData/rawNodeMetricsShort.json') //using short rn b/c I don't have up-to-date full
+const nodePercentages = require('../mockData/nodesPercentages.json')
 const k8sApi = require('../../k8sApi.js');
 const fetch = require('node-fetch');
 const podData = require('../../constructors/podConstructor.js')
-const nodeData = require('../../constructors/nodeConstructor.js')
+const nodeData = require('../../constructors/nodeConstructor.js');
+const { mockServer } = require('@graphql-tools/mock');
 
 //set to true to use mockData instead of pulling real k8s cluster data 
-const mockMode = false;
+const mockMode = true;
 const appUrl = 'http://localhost:3000'
 
 //helper function that acts as Object.assign but deeply
@@ -33,18 +38,19 @@ const mergeDeep = (target, source) => {
 module.exports = {
   Query: {
     getPods: async (parent, args, context, info) => {
+      // console.log(mock.pods);
       const pods = mockMode ? mockPods : (await k8sApi.listNamespacedPod('default')).response.body.items;
       //this is an ugly hack to pass the name and namespace context down to containers.
       //a better system would be able to access this grandparent data directly
       //a seconary strategy will be to add a conditional that only runs this loop when container data will later be queried
-      pods.forEach(pod => pod.spec.containers.forEach(container => {
-        container.podName = pod.metadata.name;
-        container.namespace = pod.metadata.namespace;
-      }))
+      // pods.forEach(pod => pod.spec.containers.forEach(container => {
+      //   container.podName = pod.metadata.name;
+      //   container.namespace = pod.metadata.namespace;
+      // }))
       return pods
     },
     getNodes: async (parent, args, context, info) => {
-      const nodes = mockMode ? mockNodes : (await k8sApi.listNode('default')).response.body.items
+      const nodes = mockMode ? mockPodMetrics : (await k8sApi.listNode('default')).response.body.items
       return nodes
     }
   },
