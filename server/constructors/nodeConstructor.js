c0c008ee559070e72d0dd32233ffc08a6ac8e2f8
Whit Rooke <whit.rooke@gmail.com>
2021-05-19 20:39:35 -0400

    pre pull commit

--- server/constructors/nodeConstructor.js
+++ server/constructors/nodeConstructor.js
@@ -1,12 +1,13 @@
 const cmd = require('node-cmd');
-
+const Parser = require('table-parser');
 const Node = {};
 
 Node.getPercentages = async function(node = ''){
   const percents = cmd.runSync(`kubectl top node ${node}`);
   let percentObj = Parser.parse(percents.data);
+  console.log(percentObj);
   percentObj = await JSON.stringify(percentObj);
   return percentObj;
-}
+};
 
 module.exports = Node;
