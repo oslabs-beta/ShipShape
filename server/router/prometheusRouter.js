d23766c5ced9d2b0e707d81883d5a19eb6fda43b
Brian Barr <brian.barr23@gmail.com>
2021-05-26 23:26:55 -0400

    testing if this is verified

--- server/router/prometheusRouter.js
+++ server/router/prometheusRouter.js
@@ -3,6 +3,7 @@ const prometheusController = require('./../controller/prometheusController.js');
 
 const prometheusRouter = express.Router();
 
+//route to deply prometheus onto the Cluster
 prometheusRouter.post('/install', (req, res) =>{
   res.status(200).json('Prometheus Fired Up')
 })
