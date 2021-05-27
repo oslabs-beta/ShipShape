1d82da9ba03d031f152af9584f790a8335570c56
ozi-oztrk <ozioztrkny@gmail.com>
2021-05-27 12:23:36 -0400

    ClusterView Added

---
+++ client/Components/Home/ClusterDashboard.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { filter, find } from "lodash";
+
+function ClusterDashboard() {
+
+  return (
+    <div className="ClusterDashboard">
+    cluster view dashboard
+    </div>
+  );
+}
+
+export default ClusterDashboard;
